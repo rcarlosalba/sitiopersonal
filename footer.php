7cd4b90c380edfce0f4fa358fe78af7a30d45022
Carlos Alba <hola@carlosalba.net>
2019-09-03 14:40:54 +0300

    header ok, trabajando en el footer

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer>
-    <ul class="list-style">
-        <li><a href="#"><i class="fab"></i></a></li>
-        <li><a href="#"><i class="fab"></i></a></li>
-        <li><a href="#"><i class="fab"></i></a></li>
-        <li><a href="#"><i class="fab"></i></a></li>
-        <li><a href="#"><i class="fab"></i></a></li>
-        <li><a href="#"><i class="fab"></i></a></li>
+    <ul class="list--style">
+        <li><a href="https://www.facebook.com/rcarlos.alba"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="https://twitter.com/rcarlosalba"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="https://www.instagram.com/rcarlosalba/"><i class="fab fa-instagram"></i></a></li>
+        <li><a href="https://github.com/rcarlosalba"><i class="fab fa-github"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/rcarlosalba/"><i class="fab fa-linkedin-in"></i></a></li>
+        <li><a href="mailto:hola@carlosalba.net?subject=Mail from Our Site"><i class="far fa-envelope"></i></a></li>
     </ul>
 </footer>
--- menu.php
+++ menu.php
@@ -1,16 +1,19 @@
-<header>
-    <nav>
+<header class="flex--between main--menu">
+    <figure>
+        <a href="#"><img class="logo" src="./img/logo_carlosalba.png" alt="Carlos Alba | Front End Developer Junior"></a>
+    </figure>
+    <nav class="navigation">
         <figure>
-            <a href="#"><img class="logo" src="./img/logo_carlosalba.png" alt="Carlos Alba | Front End Developer Junior"></a>
+            <img class="menu__img" src="./img/icon_menu.png" alt="Menu Principal">
         </figure>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="conoceme.php">Conóceme</a></li>
-            <li><a href="cv.php">CV</a></li>
-            <li><a href="cursos.php">Cursos</a></li>
-            <li><a href="portafolio">Portafolio</a></li>
-            <li><a href="http://blog.carlosalba.net/">Blog</a></li>
-            <li><a href="recursos.php">Recursos</a></li>            
+        <ul class="list--style">
+            <li class="list--item__menu"><a class="text--decoration" href="index.php">Home</a></li>
+            <li class="list--item__menu"><a class="text--decoration" href="conoceme.php">Conóceme</a></li>
+            <li class="list--item__menu"><a class="text--decoration" href="cv.php">CV</a></li>
+            <li class="list--item__menu"><a class="text--decoration" href="cursos.php">Cursos</a></li>
+            <li class="list--item__menu"><a class="text--decoration" href="portafolio">Portafolio</a></li>
+            <li class="list--item__menu"><a class="text--decoration" href="http://blog.carlosalba.net/">Blog</a></li>
+            <li class="list--item__menu"><a class="text--decoration" href="recursos.php">Recursos</a></li>            
         </ul>
     </nav>
 </header>
