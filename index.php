379de0a2882cc0b88667cf92cb541fbec42947ac
Carlos Alba <hola@carlosalba.net>
2019-09-04 13:30:43 +0300

    responsive, imagenes, tamanios, posiciones, ok

--- index.php
+++ index.php
@@ -5,22 +5,40 @@
     <div class="grid main--column">
         <div class="tres--columnas">
             <div class="uno">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eligendi nam cupiditate, harum provident ab quos aperiam at amet, ex labore voluptatum. Mollitia a enim modi incidunt accusamus facere voluptate.</p>
+                <a class="text--decoration" href="#">
+                    <h2 class="titles titleH2">Conóceme</h2>
+                    <img class="img--grid" src="./img/carlosalba.jpg" alt="Carlos Alba | Front End Developer Junior">
+                </a>
             </div>
             <div class="dos">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eligendi nam cupiditate, harum provident ab quos aperiam at amet, ex labore voluptatum. Mollitia a enim modi incidunt accusamus facere voluptate.</p>
+                <a class="text--decoration" href="#">
+                    <h2 class="titles titleH2">Curriculum</h2>
+                    <img class="img--grid" src="./img/curriculum.jpg" alt="Curriculum Vitae">
+                </a>
             </div>
             <div class="tres">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eligendi nam cupiditate, harum provident ab quos aperiam at amet, ex labore voluptatum. Mollitia a enim modi incidunt accusamus facere voluptate.</p>
+                <a class="text--decoration" href="#">
+                    <h2 class="titles titleH2">Portafolio</h2>
+                    <img class="img--grid" src="./img/portafolio.jpg" alt="Portafolio Profesional">
+                </a>
             </div>
             <div class="cuatro">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eligendi nam cupiditate, harum provident ab quos aperiam at amet, ex labore voluptatum. Mollitia a enim modi incidunt accusamus facere voluptate.</p>
+                <a class="text--decoration" href="#">
+                    <h2 class="titles titleH2">Cursos</h2>
+                    <img class="img--grid" src="./img/cursos.jpg" alt="Cursos Online">
+                </a>
             </div>
             <div class="cinco">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eligendi nam cupiditate, harum provident ab quos aperiam at amet, ex labore voluptatum. Mollitia a enim modi incidunt accusamus facere voluptate.</p>
+                <a class="text--decoration" href="#">
+                    <h2 class="titles titleH2">Blog</h2>
+                    <img class="img--grid" src="./img/blog.jpg" alt="Blog Personal">
+                </a>
             </div>
             <div class="seis">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eligendi nam cupiditate, harum provident ab quos aperiam at amet, ex labore voluptatum. Mollitia a enim modi incidunt accusamus facere voluptate.</p>               
+                <a class="text--decoration" href="#">
+                    <h2 class="titles titleH2">Recursos</h2>
+                    <img class="img--grid" src="./img/recursos.jpg" alt="Recusos Online">
+                </a>
             </div>
         </div>
     </div>
