7d76f57df9a19640d66e9a8e89b3f58d2be6750f
Carlos Alba <hola@carlosalba.net>
2020-04-19 14:57:37 +0300

    minified, nano, purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+
 module.exports = {
   plugins: [
     // ...
     require('tailwindcss'),
     require('autoprefixer'),
-    // ...
+    purgecss({
+      content: ["./**/*.html"],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    })
   ]
 }
